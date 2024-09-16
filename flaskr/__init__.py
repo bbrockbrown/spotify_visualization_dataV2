3f256f9ae1330da09ff160b96093969d7068e337
Brock Brown <brockbrown46@gmail.com>
2024-09-16 11:19:14 -0700

    Initial setup for app initialization

---
+++ flaskr/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, redirect, url_for, render_template, request, jsonify, send_from_directory, flash, session
+from flask_session import Session
+import os
+from datetime import timedelta
+
+def create_app(test_config=None):
+    # create app instance
+    # SET DEBUG TO FALSE WHEN DEPLOYING APP!!!!!!!!!!!!!!!!
+    app = Flask(__name__, instance_relative_config=True)
+    
+    # same as 'app.config[SESSION_TYPE] = ...'
+    app.config.from_mapping(
+        SESSION_TYPE='filesystem',
+        SECRET_KEY='hello',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+        
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # Initialize session with app & max time per session for users
+    Session(app)
+    app.permanent_session_lifetime = timedelta(minutes=30)
+    
+    # Initialize database with app
+    # db.init_app(app)
+    # with app.app_context():
+    #     db.create_all()
+    
+    return app
