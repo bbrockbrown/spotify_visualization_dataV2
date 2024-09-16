009201c4105d6651aaed9f8d2f33336d5a94e427
Brock Brown <brockbrown46@gmail.com>
2024-09-16 14:00:21 -0700

    Initialize admin-checking feature

--- flaskr/admin.py
+++ flaskr/admin.py
@@ -1,9 +1,46 @@
-import functools
+from functools import wraps
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 import os, uuid, glob
-
+# checks if user is in session
+from utils.session import inSession
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+# get database
+db = get_db()
+
+# function for checking if user has permission to view databases
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+                
+        # Check if the user is logged in
+        if "user" not in session:
+            return redirect(url_for('auth.login'))
+                    
+        # Check if the logged-in user is an admin
+        if not session["isAdmin"]:
+            return render_template("admin/error.html", logged_in=inSession())
+                    
+        # If the user is logged in and is an admin, proceed to the requested page
+        return f(*args, **kwargs)
+            
+    return decorated_function
+
+@bp.route('/bug-reports')
+@admin_required
+def reports():
+    # Fetch all bug reports from the database
+    reports = db.execute('SELECT * FROM bug_report').fetchall()
+    return render_template('admin/reports.html', reports=reports, logged_in=inSession())
+
+@bp.route('/users')
+@admin_required
+def users():
+    # Fetch all users from database
+    users = db.execute('SELECT * FROM user').fetchall()
+    return render_template('admin/users.html', users=users, logged_in=inSession())
+
